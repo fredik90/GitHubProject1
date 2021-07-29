cb4cd93ed9788f8160943b522d1fafc7e38b0883
Farhad.Z <86328633+fredik90@users.noreply.github.com>
2021-07-29 11:35:10 -0700

    adding one item

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -1,4 +1,13 @@
 package day3;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class ListPractice {
+    public static void main(String[] args) {
+
+        List <String> lst = new  ArrayList <>();
+
+
+    }
 }
